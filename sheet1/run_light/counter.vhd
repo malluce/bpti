0e545e818cf478d1840327e88553947faff13e4a
Capp Lab10 <capplab10@i80labpc01>
2017-11-09 12:08:09 +0100

    implemented first modus for runlight

--- sheet1/run_light/counter.vhd
+++ sheet1/run_light/counter.vhd
@@ -12,12 +12,14 @@ entity counter_ent is
 end counter_ent;
 
 architecture behaviour_counter of counter_ent is
-	 constant MIN : integer range 0 to 31250000 := 31250000;
-	 constant MAX : integer range 0 to 488281 := 488281;
+	 constant MIN_SPEED : integer range 0 to 31250000 := 31250000;
+	 constant MAX_SPEED : integer range 0 to 488281 := 488281;
 	 constant MULTIPLIER : integer range 0 to 250000 := 250000;
 	 constant SPEED_INIT : integer range 0 to 3906250 := 3906250;
     signal cnt : integer range 0 to 31250000 := 0;
-	 shared variable speed : integer := SPEED_INIT;
+	 signal speed : integer range 0 to MIN_SPEED := SPEED_INIT;
+	 signal up_button_pressed : std_logic := '0';
+	 signal down_button_pressed : std_logic := '0';
 begin
 	count_process : process(clk, rst)
 	begin
@@ -26,7 +28,7 @@ begin
 			b <= '0';
 		else
 			if clk'event and clk = '1' then
-				if cnt = speed then
+				if cnt >= speed then
 					b <= '1';
 					cnt <= 0;
 				else 
@@ -37,16 +39,24 @@ begin
 		end if;
 	end process count_process;
 	
-	speed_proc : process(speed_up, speed_down, rst)
+	speed_proc : process(clk, rst)
 	begin
 		if rst = '0' then
-			speed := SPEED_INIT;
-	   elsif  speed_up = '1' and (speed - MULTIPLIER) > MIN then
-			speed := speed - MULTIPLIER;
-		elsif speed_down ='1' and (speed + MULTIPLIER) < MAX then
-			speed := speed + MULTIPLIER;
-		else
-			speed := speed;
+			speed <= SPEED_INIT;
+			up_button_pressed <= '0';
+			down_button_pressed <= '0';
+		elsif clk'event and clk = '1' then	
+			if  speed_up = '1' and speed > MAX_SPEED and up_button_pressed = '0' then
+				speed <= speed - MULTIPLIER;
+				up_button_pressed <= '1';
+			elsif speed_down ='1' and speed < MIN_SPEED and down_button_pressed = '0' then
+				speed <= speed + MULTIPLIER;
+				down_button_pressed <= '1';	
+			elsif speed_up = '0' and up_button_pressed = '1' then
+				up_button_pressed <= '0';
+			elsif speed_down = '0' and down_button_pressed = '1' then
+				down_button_pressed <= '0';
+			end if;
 		end if;
 	end process speed_proc;
 end behaviour_counter;
--- sheet1/run_light/modi_selector.vhd
+++ sheet1/run_light/modi_selector.vhd
@@ -12,9 +12,70 @@ entity modi_selector is
 end modi_selector;
 
 architecture modi_selector_behav of modi_selector is
-
+	type DIR is (FWD, BWD);
+	type MODUS_0_STATES is (D10_D11, D9_D12, D8_D13, D7_D14);
+	signal modus_pressed : std_logic := '0';
+	signal direction_pressed : std_logic := '0';
+	
 begin
-
-
+	set_process : process(clock_sel, reset_sel)
+	variable modus_0_state : MODUS_0_STATES := D10_D11;
+	variable modus : integer range 0 to 2 := 0;
+	variable direction : DIR := FWD;
+	begin
+		if reset_sel = '0' then
+			vector_sel <= "00000000";
+			modus := 0;
+			direction := FWD;
+			modus_0_state := D10_D11;
+		else 
+			if clock_sel'event and clock_sel = '1' then
+				if  modus_sel = '1' and modus_pressed = '0' then	
+					modus_pressed <= '1';
+					modus := modus + 1;
+					MODUS_INIT : case modus is
+						when 0 => modus_0_state := D10_D11;
+						when 1 => modus_0_state := D10_D11; -- platzhalter
+						when 2 => modus_0_state := D10_D11; -- platzhalter
+					end case MODUS_INIT;
+				elsif direction_sel ='1' and direction_pressed = '0' then
+					if (direction = FWD) then
+						direction := BWD;
+					else 
+						direction := FWD;
+					end if;
+					direction_pressed <= '1';	
+				elsif modus_sel = '0' and modus_pressed = '1' then
+					modus_pressed <= '0';
+				elsif direction_sel = '0' and direction_pressed = '1' then
+					direction_pressed <= '0';
+				end if;		
+				
+				if (modus = 0 and direction = FWD) then 
+					MODUS_0_CASE_FWD : case modus_0_state is
+						when D10_D11 => modus_0_state := D9_D12; 
+											 vector_sel <= "00011000";
+						when D9_D12 => modus_0_state := D8_D13;
+											vector_sel <= "00100100";
+						when D8_D13 => modus_0_state := D7_D14;
+											vector_sel <= "01000010";
+						when D7_D14 => modus_0_state := D10_D11;
+											vector_sel <= "10000001";
+					end case MODUS_0_CASE_FWD;
+				elsif (modus = 0 and direction = BWD) then
+					MODUS_0_CASE_BWD : case modus_0_state is
+						when D10_D11 => modus_0_state := D7_D14; 
+											 vector_sel <= "00011000";
+						when D9_D12 => modus_0_state := D10_D11;
+											vector_sel <= "00100100";
+						when D8_D13 => modus_0_state := D9_D12;
+											vector_sel <= "01000010";
+						when D7_D14 => modus_0_state := D8_D13;
+											vector_sel <= "10000001";
+					end case MODUS_0_CASE_BWD;
+				end if;
+			end if;
+		end if;
+	end process set_process;
 end modi_selector_behav;
 
