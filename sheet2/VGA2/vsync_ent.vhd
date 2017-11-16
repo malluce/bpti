e14ad9b37c5454d24adde3b8c5f97740913905c0
Capp Lab10 <capplab10@i80labpc01>
2017-11-16 16:50:25 +0100

    hopefully fixed vsync

--- sheet2/VGA2/vsync_ent.vhd
+++ sheet2/VGA2/vsync_ent.vhd
@@ -14,40 +14,38 @@ end vsync_ent;
 
 architecture vsync_behav of vsync_ent is
 	signal cnt_high : integer range 0 to 524 :=  0; 
-	signal next_0 : std_logic := '0';
-	signal cnt_row : integer range 1 to 480 := 1;
+	signal set_0 : std_logic := '0';
+	signal cnt_row : integer range 0 to 479 := 0;
 begin
 	
 	vsync_proc : process(hsync_in, rst_vsync)
 	begin
 		if(rst_vsync = '0') then
 			cnt_high <= 0;
-			next_0 <= '0';
+			cnt_row <= 0;
+			set_0 <= '0';
 			vsync_out <= '1';
 			row_vsync <= "000000001";
 		elsif(hsync_in'event and hsync_in = '1') then
-			if(next_0 = '0') then
+			if(set_0 = '0') then
 				vsync_out <= '1';
-				if(cnt_high > 31 and cnt_high <= 510) then
+				
+				if(cnt_high >= 30 and cnt_high < 509) then
 					cnt_row <= cnt_row + 1;
 				end if;
-				
+
 				if(cnt_high = 523) then
-					cnt_high <= 1;
-					next_0 <= '1';
+					cnt_high <= 0;
+					set_0 <= '1';
 				else
 					cnt_high <= cnt_high + 1;
 				end if;
-			
 			else
 				vsync_out <= '0';
-				next_0 <= '0';
-				
+				set_0 <= '0';
+				cnt_row <= 0;
 			end if;
-			row_vsync <= std_logic_vector(to_unsigned(cnt_row, 9));
 		end if;
+		row_vsync <= std_logic_vector(to_unsigned(cnt_row, 9));
 	end process vsync_proc;
-
-
 end vsync_behav;
-
