5dc28a13797be718c87358264581477b987606eb
Niklas Metz <niklasmetz@gmx.de>
2018-02-03 14:53:16 +0100

    Added a graphics entity and made a lot of consitency changes.

--- project/bomberman_ise/bomberman.vhd
+++ project/bomberman_ise/bomberman.vhd
@@ -85,85 +85,46 @@ architecture bomberman_struct of bomberman_ent is
         );
     end component;
 
-    component pixel_gen_ent
-		generic(PLAYER_SIZE_PIX, TILE_SIZE_PIX : integer);
+    component graphics_ent
+        generic(PLAYER_SIZE_GRAPHICS, TILE_SIZE_GRAPHICS : integer);
         port(
-            row_pixel : in std_logic_vector(8 downto 0);
-            col_pixel : in std_logic_vector(9 downto 0);
-            p1_x_coord_pixel : in std_logic_vector(8 downto 0);
-            p1_y_coord_pixel : in std_logic_vector(8 downto 0);
-            p1_enable_pixel : in std_logic;
-            p2_x_coord_pixel : in std_logic_vector(8 downto 0);
-            p2_y_coord_pixel : in std_logic_vector(8 downto 0);
-            p2_enable_pixel : in std_logic;
-            row0_pixel : in std_logic_vector(59 downto 0);
-            row1_pixel : in std_logic_vector(59 downto 0);
-            row2_pixel : in std_logic_vector(59 downto 0);
-            row3_pixel : in std_logic_vector(59 downto 0);
-            row4_pixel : in std_logic_vector(59 downto 0);
-            row5_pixel : in std_logic_vector(59 downto 0);
-            row6_pixel : in std_logic_vector(59 downto 0);
-            row7_pixel : in std_logic_vector(59 downto 0);
-            row8_pixel : in std_logic_vector(59 downto 0);
-            row9_pixel : in std_logic_vector(59 downto 0);
-            row10_pixel : in std_logic_vector(59 downto 0);
-            row11_pixel : in std_logic_vector(59 downto 0);
-			row12_pixel : in std_logic_vector(59 downto 0);
-			row13_pixel : in std_logic_vector(59 downto 0);
-			row14_pixel : in std_logic_vector(59 downto 0);
-            sprite_id_pixel : out std_logic_vector(3 downto 0);
-			sprite_row_pixel : out std_logic_vector(4 downto 0);
-			sprite_col_pixel : out std_logic_vector(4 downto 0);
-			player_id_pixel : out std_logic_vector(3 downto 0);
-			player_x_pixel : out std_logic_vector(4 downto 0);
-			player_y_pixel : out std_logic_vector(4 downto 0)
+            row_graphics : in std_logic_vector(8 downto 0);
+            col_graphics : in std_logic_vector(9 downto 0);
+            p1_x_coord_graphics : in std_logic_vector(8 downto 0);
+            p1_y_coord_graphics : in std_logic_vector(8 downto 0);
+            p1_enable_graphics : in std_logic;
+            p2_x_coord_graphics : in std_logic_vector(8 downto 0);
+            p2_y_coord_graphics : in std_logic_vector(8 downto 0);
+            p2_enable_graphics : in std_logic;
+            row0_graphics : in std_logic_vector(59 downto 0);
+            row1_graphics : in std_logic_vector(59 downto 0);
+            row2_graphics : in std_logic_vector(59 downto 0);
+            row3_graphics : in std_logic_vector(59 downto 0);
+            row4_graphics : in std_logic_vector(59 downto 0);
+            row5_graphics : in std_logic_vector(59 downto 0);
+            row6_graphics : in std_logic_vector(59 downto 0);
+            row7_graphics : in std_logic_vector(59 downto 0);
+            row8_graphics : in std_logic_vector(59 downto 0);
+            row9_graphics : in std_logic_vector(59 downto 0);
+            row10_graphics : in std_logic_vector(59 downto 0);
+            row11_graphics : in std_logic_vector(59 downto 0);
+            row12_graphics : in std_logic_vector(59 downto 0);
+            row13_graphics : in std_logic_vector(59 downto 0);
+            row14_graphics : in std_logic_vector(59 downto 0);
+            red_0_graphics : out std_logic;
+            red_1_graphics : out std_logic;
+            red_2_graphics : out std_logic;
+            red_3_graphics : out std_logic;
+            green_0_graphics : out std_logic;
+            green_1_graphics : out std_logic;
+            green_2_graphics : out std_logic;
+            green_3_graphics : out std_logic;
+            blue_0_graphics : out std_logic;
+            blue_1_graphics : out std_logic;
+            blue_2_graphics : out std_logic;
+            blue_3_graphics : out std_logic
         );
     end component;
-
-    component rgb_assign_ent
-        port(
-            red_assign : in std_logic_vector(3 downto 0);
-            green_assign : in std_logic_vector(3 downto 0);
-            blue_assign : in std_logic_vector(3 downto 0);
-            red_assign_0 : out std_logic;
-            red_assign_1 : out std_logic;
-            red_assign_2 : out std_logic;
-            red_assign_3 : out std_logic;
-            green_assign_0 : out std_logic;
-            green_assign_1 : out std_logic;
-            green_assign_2 : out std_logic;
-            green_assign_3 : out std_logic;
-            blue_assign_0 : out std_logic;
-            blue_assign_1 : out std_logic;
-            blue_assign_2 : out std_logic;
-            blue_assign_3 : out std_logic
-        );
-    end component;
-
-	component SpriteROM
-		port(
-			sprite_id : in std_logic_vector(3 downto 0);
-			sprite_row : in std_logic_vector(4 downto 0);
-			sprite_col : in std_logic_vector(4 downto 0);
-			red_sprite : out std_logic_vector(3 downto 0);
-			green_sprite : out std_logic_vector(3 downto 0);
-			blue_sprite : out std_logic_vector(3 downto 0)
-		);
-	end component;
-
-	component PlayerROM
-		port(
-			id_player : in std_logic_vector(3 downto 0);
-			x_player : in std_logic_vector(4 downto 0);
-			y_player : in std_logic_vector(4 downto 0);
-			red_in_player : in std_logic_vector(3 downto 0);
-			green_in_player : in std_logic_vector(3 downto 0);
-			blue_in_player : in std_logic_vector(3 downto 0);
-			red_out_player : out std_logic_vector(3 downto 0);
-			green_out_player : out std_logic_vector(3 downto 0);
-			blue_out_player : out std_logic_vector(3 downto 0)
-		);
-	end component;
 
 	constant TILE_SIZE : integer range 0 to 32 := 32; -- size of a single tile
 	constant PLAYER_SIZE : integer range 0 to TILE_SIZE := 32; --size of the player
@@ -192,18 +153,6 @@ architecture bomberman_struct of bomberman_ent is
 	signal row12_fwd : std_logic_vector(59 downto 0);
 	signal row13_fwd : std_logic_vector(59 downto 0);
 	signal row14_fwd : std_logic_vector(59 downto 0);
-    signal red_fwd : std_logic_vector(3 downto 0);
-    signal green_fwd : std_logic_vector(3 downto 0);
-    signal blue_fwd : std_logic_vector(3 downto 0);
-	signal red_player_fwd : std_logic_vector(3 downto 0);
-    signal green_player_fwd : std_logic_vector(3 downto 0);
-    signal blue_player_fwd : std_logic_vector(3 downto 0);
-	signal sprite_id_fwd : std_logic_vector(3 downto 0);
-	signal sprite_row_fwd : std_logic_vector(4 downto 0);
-	signal sprite_col_fwd : std_logic_vector(4 downto 0);
-	signal player_id_sprite_fwd : std_logic_vector(3 downto 0);
-	signal player_x_sprite_fwd : std_logic_vector(4 downto 0);
-	signal player_y_sprite_fwd : std_logic_vector(4 downto 0);
 
 begin
     sync : sync_gen_ent port map(
@@ -253,9 +202,9 @@ begin
 		row14_fwd
     );
 
-    pixel : pixel_gen_ent
-	generic map(PLAYER_SIZE, TILE_SIZE)
-	port map(
+    graphics : graphics_ent
+    generic map(PLAYER_SIZE, TILE_SIZE)
+    port map(
         row_fwd,
         col_fwd,
         p1_x_coord_fwd,
@@ -276,21 +225,9 @@ begin
         row9_fwd,
         row10_fwd,
         row11_fwd,
-		row12_fwd,
-		row13_fwd,
-		row14_fwd,
-        sprite_id_fwd,
-        sprite_row_fwd,
-        sprite_col_fwd,
-		player_id_sprite_fwd,
-		player_x_sprite_fwd,
-		player_y_sprite_fwd
-    );
-
-    rgb_assign : rgb_assign_ent port map(
-        red_fwd,
-        green_fwd,
-        blue_fwd,
+        row12_fwd,
+        row13_fwd,
+        row14_fwd,
         red_0,
         red_1,
         red_2,
@@ -305,25 +242,5 @@ begin
         blue_3
     );
 
-	 board_sprites : SpriteROM port map(
-        sprite_id_fwd,
-        sprite_row_fwd,
-        sprite_col_fwd,
-		red_player_fwd,
-		green_player_fwd,
-		blue_player_fwd
-	 );
-
-	 player_sprites : PlayerROM port map(
-		player_id_sprite_fwd,
-		player_x_sprite_fwd,
-		player_y_sprite_fwd,
-		red_player_fwd,
-		green_player_fwd,
-		blue_player_fwd,
-		red_fwd,
-		green_fwd,
-		blue_fwd
-	 );
 
 end bomberman_struct;
--- project/bomberman_ise/rgb_assign_ent.vhd
+++ project/bomberman_ise/rgb_assign_ent.vhd
@@ -6,18 +6,18 @@ entity rgb_assign_ent is
 		red_assign : in std_logic_vector(3 downto 0);
 		green_assign : in std_logic_vector(3 downto 0);
 		blue_assign : in std_logic_vector(3 downto 0);
-		red_assign_0 : out std_logic;
-		red_assign_1 : out std_logic;
-		red_assign_2 : out std_logic;
-		red_assign_3 : out std_logic;
-		green_assign_0 : out std_logic;
-		green_assign_1 : out std_logic;
-		green_assign_2 : out std_logic;
-		green_assign_3 : out std_logic;
-		blue_assign_0 : out std_logic;
-		blue_assign_1 : out std_logic;
-		blue_assign_2 : out std_logic;
-		blue_assign_3 : out std_logic
+		red_0_assign : out std_logic;
+		red_1_assign : out std_logic;
+		red_2_assign : out std_logic;
+		red_3_assign : out std_logic;
+		green_0_assign : out std_logic;
+		green_1_assign : out std_logic;
+		green_2_assign : out std_logic;
+		green_3_assign : out std_logic;
+		blue_0_assign : out std_logic;
+		blue_1_assign : out std_logic;
+		blue_2_assign : out std_logic;
+		blue_3_assign : out std_logic
 	);
 end rgb_assign_ent;
 
@@ -27,18 +27,18 @@ begin
 	assign_proc : process(red_assign, green_assign, blue_assign)
 	begin
 		--set the individual bits from the input vectors
-		red_assign_0 <= red_assign(0);
-		red_assign_1 <= red_assign(1);
-		red_assign_2 <= red_assign(2);
-		red_assign_3 <= red_assign(3);
-		green_assign_0 <= green_assign(0);
-		green_assign_1 <= green_assign(1);
-		green_assign_2 <= green_assign(2);
-		green_assign_3 <= green_assign(3);
-		blue_assign_0 <= blue_assign(0);
-		blue_assign_1 <= blue_assign(1);
-		blue_assign_2 <= blue_assign(2);
-		blue_assign_3 <= blue_assign(3);
+		red_0_assign <= red_assign(0);
+		red_1_assign <= red_assign(1);
+		red_2_assign <= red_assign(2);
+		red_3_assign <= red_assign(3);
+		green_0_assign <= green_assign(0);
+		green_1_assign <= green_assign(1);
+		green_2_assign <= green_assign(2);
+		green_3_assign <= green_assign(3);
+		blue_0_assign <= blue_assign(0);
+		blue_1_assign <= blue_assign(1);
+		blue_2_assign <= blue_assign(2);
+		blue_3_assign <= blue_assign(3);
 	end process assign_proc;
 
 end rgb_assign_behav;
