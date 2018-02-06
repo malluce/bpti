3e248cb30a3041498f85c312a0ce7e300b8e824a
Felix Bachmann <felix.bachmann@ewetel.net>
2018-02-06 20:20:02 +0100

    hopefully fixed movement problem for up/down

--- project/bomberman_ise/movement.vhd
+++ project/bomberman_ise/movement.vhd
@@ -24,21 +24,25 @@ architecture movement_behav of movement_ent is
 	shared variable x_int : integer range 0 to 480 := X_INIT_MOVE;
 	shared variable y_int : integer range 0 to 480 := Y_INIT_MOVE;
 	shared variable x_right : integer range 0 to 480 := X_INIT_MOVE + PLAYER_SIZE_MOVE - 1;
+
+	-- the upper/lower tile which may cause collision when moving left/right
 	shared variable upper_tile : std_logic_vector(3 downto 0);
 	shared variable lower_tile : std_logic_vector(3 downto 0);
+
+	-- the left/right tile which may cause collision when moving up/down
 	shared variable left_tile : std_logic_vector(3 downto 0);
-	shared variable mid_tile : std_logic_vector(3 downto 0);
 	shared variable right_tile : std_logic_vector(3 downto 0);
+
+	-- index for accessing left/right upper/lower tile from a row vector
 	shared variable row_idx_left : integer range 0 to 59;
-	shared variable row_idx_mid : integer range 0 to 59;
 	shared variable row_idx_right : integer range 0 to 59;
-	
+
 begin
 
 	move_up_down : process(clk_move, rst_move)
 
-	
-	
+
+
 	variable speed : integer range 0 to 5 := 1;
 
 	begin
@@ -47,52 +51,38 @@ begin
 			speed := 1;
 		elsif(clk_move'event and clk_move = '1') then
 			row_idx_left := 59 - (x_int-1)/TILE_SIZE_MOVE * 4;
-			row_idx_mid := 59 - (x_right)/TILE_SIZE_MOVE * 4;
-			row_idx_right := 59 - (x_right+1)/TILE_SIZE_MOVE * 4;
-			left_tile := row_upper_move(row_idx_left downto row_idx_left - 3);
-			mid_tile := row_upper_move(row_idx_mid downto row_idx_mid - 3);
-			right_tile := row_upper_move(row_idx_right downto row_idx_right - 3);
+			row_idx_right := 59 - (x_right)/TILE_SIZE_MOVE * 4;
 			if(up_move='0') then
-				if((y_int - 1) mod TILE_SIZE_MOVE /= 0) then -- no row change
+				if((y_int - 1) mod TILE_SIZE_MOVE /= 0) then -- no row change throug moving
 					y_int := y_int - speed;
 				else
-					if((x_int - 1) mod TILE_SIZE_MOVE = 0) then -- player is column-wise tile aligned, just need to check mid tile
-						if(to_integer(unsigned(mid_tile)) /= to_integer(unsigned(BOMB_CODING))) then
+					left_tile := row_upper_move(row_idx_left downto row_idx_left - 3);
+					right_tile := row_upper_move(row_idx_right downto row_idx_right - 3);
+					if((x_int - 1) mod TILE_SIZE_MOVE = 0) then -- player is column-wise tile aligned, just need to check left tile
+						if(to_integer(unsigned(left_tile)) /= to_integer(unsigned(BOMB_CODING))) then -- check collision
 							y_int := y_int - speed;
 						end if;
 					else -- need to check two tiles
-						if((x_int + (PLAYER_SIZE_MOVE/2 - 1)) /TILE_SIZE_MOVE > x_int/TILE_SIZE_MOVE) then
-							left_tile := row_upper_move(row_idx_left downto row_idx_left - 3);
-							right_tile := row_upper_move(row_idx_mid downto row_idx_mid - 3);
-						else
-							left_tile := row_upper_move(row_idx_mid downto row_idx_mid - 3);
-							right_tile := row_upper_move(row_idx_right downto row_idx_right - 3);
-						end if;
-						if(to_integer(unsigned(left_tile))  < to_integer(unsigned(BOMB_CODING)) and to_integer(unsigned(right_tile))  < to_integer(unsigned(BOMB_CODING))) then -- next tile is allowed
+						if(to_integer(unsigned(left_tile))  < to_integer(unsigned(BOMB_CODING)) and to_integer(unsigned(right_tile))  < to_integer(unsigned(BOMB_CODING))) then -- check collision
 							y_int := y_int - speed;
 						end if;
-					end if;			
+					end if;
 				end if;
 			elsif(down_move= '0') then
 				if((y_int + PLAYER_SIZE_MOVE - 1) mod TILE_SIZE_MOVE /= 0) then -- no row change
 					y_int := y_int + speed;
 				else
-					if((x_int - 1) mod TILE_SIZE_MOVE = 0) then -- player is column-wise tile aligned, just need to check mid tile
-						if(to_integer(unsigned(mid_tile)) /= to_integer(unsigned(BOMB_CODING))) then
+					left_tile := row_lower_move(row_idx_left downto row_idx_left - 3);
+					right_tile := row_lower_move(row_idx_right downto row_idx_right - 3);
+					if((x_int - 1) mod TILE_SIZE_MOVE = 0) then -- player is column-wise tile aligned, just need to check left tile
+						if(to_integer(unsigned(left_tile)) /= to_integer(unsigned(BOMB_CODING))) then
 							y_int := y_int + speed;
 						end if;
 					else -- need to check two tiles
-						if((x_int + (PLAYER_SIZE_MOVE/2 - 1)) /TILE_SIZE_MOVE > x_int/TILE_SIZE_MOVE) then
-							left_tile := row_upper_move(row_idx_left downto row_idx_left - 3);
-							right_tile := row_upper_move(row_idx_mid downto row_idx_mid - 3);
-						else
-							left_tile := row_upper_move(row_idx_mid downto row_idx_mid - 3);
-							right_tile := row_upper_move(row_idx_right downto row_idx_right - 3);
-						end if;
-						if(to_integer(unsigned(left_tile))  < to_integer(unsigned(BOMB_CODING)) and to_integer(unsigned(right_tile))  < to_integer(unsigned(BOMB_CODING))) then -- next tile is allowed
+						if(to_integer(unsigned(left_tile))  < to_integer(unsigned(BOMB_CODING)) and to_integer(unsigned(right_tile))  < to_integer(unsigned(BOMB_CODING))) then -- check collision
 							y_int := y_int + speed;
 						end if;
-					end if;			
+					end if;
 				end if;
 			end if;
 			y_move <= std_logic_vector(to_unsigned(y_int, 9));
@@ -101,9 +91,9 @@ begin
 
 
 	move_left_right : process(clk_move, rst_move)
-	
+
 	variable row_idx : integer range 0 to 59;
-	
+
 	variable speed : integer range 0 to 5 := 1;
 
 	begin
@@ -125,7 +115,7 @@ begin
 							-- player is in two tiles, need to check both
 							if((y_int + (PLAYER_SIZE_MOVE/2 - 1)) /TILE_SIZE_MOVE = y_int/TILE_SIZE_MOVE) then
 								upper_tile := row_upper_move(row_idx downto row_idx - 3);
-								lower_tile := row_mid_move(row_idx downto row_idx - 3);			
+								lower_tile := row_mid_move(row_idx downto row_idx - 3);
 							else
 								upper_tile := row_mid_move(row_idx downto row_idx - 3);
 								lower_tile := row_lower_move(row_idx downto row_idx - 3);
@@ -149,7 +139,7 @@ begin
 							-- player is in two tiles, need to check both
 							if((y_int + (PLAYER_SIZE_MOVE/2 - 1)) /TILE_SIZE_MOVE = y_int/TILE_SIZE_MOVE) then
 								upper_tile := row_upper_move(row_idx downto row_idx - 3);
-								lower_tile := row_mid_move(row_idx downto row_idx - 3);			
+								lower_tile := row_mid_move(row_idx downto row_idx - 3);
 							else
 								upper_tile := row_mid_move(row_idx downto row_idx - 3);
 								lower_tile := row_lower_move(row_idx downto row_idx - 3);
